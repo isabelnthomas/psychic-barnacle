de13918979c99ae0cb1525fe66380f903f83dddd
isabelnthomas <43648535+isabelnthomas@users.noreply.github.com>
2018-11-26 16:06:13 +0000

    Update testing.R

--- rtraining/testing.R
+++ rtraining/testing.R
@@ -1,4 +1,6 @@
 #this is a file
 
 #this file has been updated
-my_vector <- c("a", "b", "c")
+my_vector <- c("a", "b", "c", "d")
+
+#edited file within github - and added in a new value to my_vector
